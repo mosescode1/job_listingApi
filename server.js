9c40611c7453d0aab6d2ae8974ba966ed873b36e
mosescode1 <emosescode@gmail.com>
2024-10-02 09:03:37 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: '.env' });
+
+
+process.on("UncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log('Uncaught Exception Shutting down');
+
+})
+const app = require('./src/app');
+
+
+
+process.on("ReferenceError", (err) => {
+  console.log(err.name, err.message);
+  console.log('Reference Error Shutting Down')
+})
+
+const PORT = process.env.PORT || 4001;
+
+app.listen(PORT, () => {
+  console.log(`App is running on Port ${PORT}`);
+});
