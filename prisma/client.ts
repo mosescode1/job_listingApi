4de33cc84d13de83b588269b2858fdf8f2eea474
mosescode1 <emosescode@gmail.com>
2025-01-08 16:10:57 +0100

    Add Prisma client configuration with password omission for security

---
+++ prisma/client.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client';
+
+export const prisma = new PrismaClient({
+	omit: {
+		jobSeeker: {
+			password: true,
+		},
+		admin: {
+			password: true,
+		},
+		employer: {
+			password: true,
+			refreshToken: true,
+		},
+	},
+});
